40baf4906a06e9ae103962c02642446e9b984a3d
1TAEKIM <oneteak1@gmail.com>
2024-11-02 18:18:40 +0900

    Add user information details

--- backend/models.py
+++ backend/models.py
@@ -5,6 +5,8 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+    favorite_food = db.Column(db.String(50), nullable=True)  
+    spice_level = db.Column(db.Integer, nullable=True)
 
     def set_password(self, password):
         self.password = bcrypt.generate_password_hash(password).decode('utf-8')
